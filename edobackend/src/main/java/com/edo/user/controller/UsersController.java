e4ffe6401e80e87d584bac4fdd65dfdfead8537a
MinJu-A <pinokoiandmj@naver.com>
2022-09-22 02:35:03 +0900

    로그인/회원가입을 위한 작업

--- edobackend/src/main/java/com/edo/user/controller/UsersController.java
+++ edobackend/src/main/java/com/edo/user/controller/UsersController.java
@@ -2,13 +2,14 @@ package com.edo.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @CrossOrigin
 @Controller
 @RequiredArgsConstructor
-public class LoginController {
+public class UsersController {
 
     @GetMapping(value="/login")
     public String Login(){
@@ -22,14 +23,23 @@ public class LoginController {
     }
 
 // 이메일 등록
-    @GetMapping(value="/memberjoinEmail")
-    public String MemberjoinEmail(){
-        return "member/memberjoinEmail";
+    @GetMapping(value="/memberjoinInfo")
+    public String MemberjoinInfo(){
+        return "memberjoinInfo";
     }
 
-    // 이메일 등록
+    // 마이페이지
     @GetMapping(value="/mypage")
     public String Mypage(){
         return "mypage/mypageMain";
     }
+
+    //로그인 실패 시 에러 메세지 나타냄
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 다시 확인해주세요");
+        return "/login";
+    }
 }
+
+
