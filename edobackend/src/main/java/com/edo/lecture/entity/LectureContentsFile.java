8cb8063217e43285b88df45769508ed8d1ca3d40
Park-HyoungJoon <diqzkvhfpqj@gmail.com>
2022-09-28 13:32:26 +0900

    파일업로드는 가능,file 부분에서 Lecturecontents_id값이 이상하게 넘어오는 부분 수정중

--- edobackend/src/main/java/com/edo/lecture/entity/LectureContentsFile.java
+++ edobackend/src/main/java/com/edo/lecture/entity/LectureContentsFile.java
@@ -22,19 +22,9 @@ public class LectureContentsFile extends BaseFileEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "lecture_contents_file_id")
     private Long id;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "lecture_contents_id")
     private LectureContents lectureContents;
 
-
-    public LectureContentsFile toLectureContentsFile(String originalFileName,long fileSize,String fileLoc,String fileType){
-        LectureContentsFile lectureContentsFile = new LectureContentsFile();
-        lectureContentsFile.setFileName(originalFileName);
-        lectureContentsFile.setFileLoc(fileLoc);
-        lectureContentsFile.setFileType(fileType);
-        lectureContentsFile.setFileSize(fileSize);
-        return lectureContentsFile;
-    }
 }
 
--- edobackend/src/main/java/com/edo/lecture/service/LectureContentsService.java
+++ edobackend/src/main/java/com/edo/lecture/service/LectureContentsService.java
@@ -1,6 +1,8 @@
 package com.edo.lecture.service;
 
 import com.edo.lecture.entity.LectureContents;
+import com.edo.lecture.entity.LectureContentsFile;
+import com.edo.lecture.repository.LectureContentsFileRepository;
 import com.edo.lecture.repository.LectureContentsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,9 @@ import org.springframework.stereotype.Service;
 public class LectureContentsService {
     @Autowired
     LectureContentsRepository lectureContentsRepository;
+
+    @Autowired
+    LectureContentsFileRepository lectureContentsFileRepository;
     public  void save(String originalFileName) {
     }
 
@@ -16,5 +21,11 @@ public class LectureContentsService {
         lectureContentsRepository.save(lectureContents);
     }
 
-    public int getNewContents(){return lectureContentsRepository.newContents();}
+    //Contents 가장 최근 ID값 검색
+    public Long getNewContents(){return lectureContentsRepository.newContents();}
+
+    public LectureContents getContentsById(long id){
+        return lectureContentsRepository.findById(id).get();
+    }
+    public void save(LectureContentsFile lectureContentsFile){lectureContentsFileRepository.save(lectureContentsFile);}
 }
