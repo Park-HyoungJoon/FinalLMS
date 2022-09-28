d03c70aac70ec463550500f2c8f2886dda730906
Park-HyoungJoon <diqzkvhfpqj@gmail.com>
2022-09-28 10:17:36 +0900

    파일 업로드에서 파일 정보를 보내는 부분까지 성공. 이 후 파일 업로드 여부 -> lecture.html에 read하는 과정 진행할 예정

--- edobackend/src/main/java/com/edo/lecture/repository/LectureContentsRepository.java
+++ edobackend/src/main/java/com/edo/lecture/repository/LectureContentsRepository.java
@@ -3,8 +3,11 @@ package com.edo.lecture.repository;
 import com.edo.lecture.entity.LectureContents;
 import com.edo.lecture.entity.LectureDivide;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LectureContentsRepository extends JpaRepository<LectureContents,Long> {
+    @Query(value = "select MAX(lecture_contents_id) from lecture_contents ORDER BY lecture_contents_id desc",nativeQuery = true)
+    int newContents();
 }
