0a8ab3963b0753288d4738705d4d44128aac1168
MinJu-A <pinokoiandmj@naver.com>
2022-10-07 02:19:29 +0900

    변경된 layout 적용

--- edobackend/src/main/java/com/edo/EdobackendApplication.java
+++ edobackend/src/main/java/com/edo/EdobackendApplication.java
@@ -3,6 +3,7 @@ package com.edo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //exclude = { SecurityAutoConfiguration.class }는 security 기본 로그인 화면 안 나오게 하는 설정
 @SpringBootApplication()
@@ -10,6 +11,7 @@ public class EdobackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdobackendApplication.class, args);
+
 	}
 
 }
