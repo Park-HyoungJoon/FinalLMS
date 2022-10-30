4d55c6973f0ba97e0b32fb7df7de4d2fecccffda
Park-HyoungJoon <diqzkvhfpqj@gmail.com>
2022-10-30 17:58:43 +0900

    메인페이지 연결부분 수정

--- edobackend/src/main/java/com/edo/community/repository/CommunityTestRepository.java
+++ edobackend/src/main/java/com/edo/community/repository/CommunityTestRepository.java
@@ -2,11 +2,14 @@ package com.edo.community.repository;
 
 import com.edo.community.entity.CommunityTest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CommunityTestRepository extends JpaRepository<CommunityTest,Long> {
 
+    @Query(value = "select * from community_test order by community_id desc LIMIT ?1" , nativeQuery = true)
+    List<CommunityTest> findDescCommunity(int limit_num);
 //    id 오름차순 조회
 //    id로 조회할 예정이니까 매개변수가 아닌 쿼리메소드에 id를 넣는다
     List<CommunityTest> findAllByOrderByCommunityId();
--- edobackend/src/main/java/com/edo/main/MainController.java
+++ edobackend/src/main/java/com/edo/main/MainController.java
@@ -1,18 +1,38 @@
 package com.edo.main;
 
+import com.edo.community.dto.CommunityTestDto;
+import com.edo.community.entity.CommunityTest;
+import com.edo.community.repository.CommunityTestRepository;
+import com.edo.lecture.entity.Lecture;
+import com.edo.lecture.repository.LectureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @CrossOrigin
 @Controller
 @RequiredArgsConstructor
 public class MainController {
+	@Autowired
+	LectureRepository lectureRepository;
+
+	@Autowired
+	CommunityTestRepository communityTestRepository;
 
 	@GetMapping(value="/")
-	public String Home(){
+	public String Home(Model model)
+	{
+		List<CommunityTest> communityMainList = communityTestRepository.findDescCommunity(4);
+		model.addAttribute("communityMainList",communityMainList);
+
+		List<Lecture> lectureList =  lectureRepository.findAllLectureTopFour(4);
+		model.addAttribute("lectureList",lectureList);
 		return "index";
 	}
 
