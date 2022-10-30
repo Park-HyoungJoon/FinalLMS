f99ec187967c56762df5172057e592ed987e52fd
MinJu-A <pinokoiandmj@naver.com>
2022-10-30 18:38:25 +0900

    커뮤니티 수정 중

--- edobackend/src/main/java/com/edo/community/entity/Community.java
+++ edobackend/src/main/java/com/edo/community/entity/Community.java
@@ -26,11 +26,11 @@ public class Community extends BaseTimeEntity {
     @JoinColumn(name ="member_id" )
     private Member id;
 
-////    외래키로 users의 nickname을 받아온다.
-////    referencedColumnName = PK 가 아닌 필드를 참조할 때 사용하는 옵션
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "nickname", referencedColumnName = "nickname")
-//    private Member nickname;
+//    외래키로 users의 nickname을 받아온다.
+//    referencedColumnName = PK 가 아닌 필드를 참조할 때 사용하는 옵션
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nickname", referencedColumnName = "nickname")
+    private Member nickname;
 
 //    제목
     @Column
@@ -40,9 +40,9 @@ public class Community extends BaseTimeEntity {
     @Column
     private String content;
 
-////    조회수
-//    @Column(name = "community_hits")
-//    private int hits;
+//    조회수
+    @Column(name = "community_hits")
+    private int hits;
 
 ////    삭제 여부 버튼
 //    @Column(name = "community_deleteYn", length = 1)
