5306a333830d7ac1fce50a79d6be6ea53a554f26
Park-HyoungJoon <diqzkvhfpqj@gmail.com>
2022-11-08 15:53:37 +0900

    강좌 추가 부분 에러 수정

--- edobackend/src/main/java/com/edo/util/file/FileVO.java
+++ edobackend/src/main/java/com/edo/util/file/FileVO.java
@@ -15,53 +15,53 @@ import java.util.List;
 public class FileVO {
     @JsonProperty("firstDivideId")
     private String firstDivideId;
+    private MultipartFile lectureContentsFile0;
     private MultipartFile lectureContentsFile1;
     private MultipartFile lectureContentsFile2;
     private MultipartFile lectureContentsFile3;
     private MultipartFile lectureContentsFile4;
-    private MultipartFile lectureContentsFile5;
 
     private MultipartFile shorts;
 
+    private String lectureContentsLink0;
     private String lectureContentsLink1;
     private String lectureContentsLink2;
     private String lectureContentsLink3;
     private String lectureContentsLink4;
-    private String lectureContentsLink5;
+    private LectureContents lectureContentsList0;
     private LectureContents lectureContentsList1;
     private LectureContents lectureContentsList2;
     private LectureContents lectureContentsList3;
     private LectureContents lectureContentsList4;
-    private LectureContents lectureContentsList5;
     //JsonProperty를 사용하는 이유는 hidden type의 경우 json 형태로 날라오기 때문에 타입을 String으로 변경하기 위해 사용
     @JsonProperty("ContentsSeq")
     private String ContentsSeq;
     public List<LectureContents> ContentsToList(FileVO fileVO){
         List<LectureContents> lectureContentsList = new ArrayList<>();
+        lectureContentsList.add(fileVO.getLectureContentsList0());
         lectureContentsList.add(fileVO.getLectureContentsList1());
         lectureContentsList.add(fileVO.getLectureContentsList2());
         lectureContentsList.add(fileVO.getLectureContentsList3());
         lectureContentsList.add(fileVO.getLectureContentsList4());
-        lectureContentsList.add(fileVO.getLectureContentsList5());
         return lectureContentsList;
     }
     public List<MultipartFile> ContentsFileToList(FileVO fileVO){
         List<MultipartFile> list = new ArrayList<>();
 
+        list.add(fileVO.getLectureContentsFile0());
         list.add(fileVO.getLectureContentsFile1());
         list.add(fileVO.getLectureContentsFile2());
         list.add(fileVO.getLectureContentsFile3());
         list.add(fileVO.getLectureContentsFile4());
-        list.add(fileVO.getLectureContentsFile5());
         return list;
     }
     public List<String> ContentsLinkToList(FileVO fileVO){
         List<String> list = new ArrayList<>();
+        list.add(fileVO.getLectureContentsLink0());
         list.add(fileVO.getLectureContentsLink1());
         list.add(fileVO.getLectureContentsLink2());
         list.add(fileVO.getLectureContentsLink3());
         list.add(fileVO.getLectureContentsLink4());
-        list.add(fileVO.getLectureContentsLink5());
         return list;
     }
 }
